b2b6c2e726c1c13c6e7e20e30d1df264df31a2c9
jemgdev <jemgdev@gmail.com>
2022-06-27 19:00:32 -0500

    Adding GITHUB API KEY

--- src/services/gitHubProjects.ts
+++ src/services/gitHubProjects.ts
@@ -1,7 +1,11 @@
 import IGitHubProject from '../models/IGitHubProject'
 
 export const getProjects = async (): Promise<IGitHubProject[]> => {
-  const response = await fetch(`${import.meta.env.VITE_GITHUB_API}`)
+  const response = await fetch(`${import.meta.env.VITE_GITHUB_API}`, {
+    headers: {
+      'Authorization': `token ${import.meta.env.VITE_GITHUB_API_KEY}`
+    }
+  })
   const data: IGitHubProject[] = await response.json()
   return data
 }
