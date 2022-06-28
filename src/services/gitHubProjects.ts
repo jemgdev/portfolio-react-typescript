4af448a318d3d8a7b01b49f2e68068644c67c0b6
jemgdev <jemgdev@gmail.com>
2022-06-27 19:30:06 -0500

    Config API repo

--- src/services/gitHubProjects.ts
+++ src/services/gitHubProjects.ts
@@ -3,7 +3,8 @@ import IGitHubProject from '../models/IGitHubProject'
 export const getProjects = async (): Promise<IGitHubProject[]> => {
   const response = await fetch(`${import.meta.env.VITE_GITHUB_API}`, {
     headers: {
-      'Authorization': `token ${import.meta.env.VITE_GITHUB_API_KEY}`
+      'Authorization': `token ${import.meta.env.VITE_GITHUB_API_KEY}`,
+      'Accept': 'application/vnd.github.v3+json'
     }
   })
   const data: IGitHubProject[] = await response.json()
