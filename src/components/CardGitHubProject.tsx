5a625af9c77dbb9a76a6dbf109c4485dec4311b2
jemgdev <jemgdev@gmail.com>
2022-06-29 11:44:35 -0500

    Page GitHubProjects finished

--- src/components/CardGitHubProject.tsx
+++ src/components/CardGitHubProject.tsx
@@ -10,6 +10,17 @@ import {
 import IGitHubProject from '../models/IGitHubProject'
 
 export default function CardGitHubProject ({ description, html_url, language, name }: IGitHubProject) {
+
+  const colorByLanguage = (languageName: string): string | undefined => {
+    if (languageName === 'JavaScript') return 'yellow'
+    else if (languageName === 'CSS') return 'messenger'
+    else if (languageName === 'Java') return 'orange'
+    else if (languageName === 'HTML') return 'red'
+    else if (languageName === 'Handlebars') return 'yellow'
+    else if (languageName === 'TypeScript') return 'blue'
+    else return 'white'
+  }
+
   return (
     <Box maxW='lg' borderWidth='1px' borderRadius='lg' overflow='hidden' shadow='md' display='flex' flexDirection='column' justifyContent='space-between'>
       <Box p='4'>
@@ -19,7 +30,7 @@ export default function CardGitHubProject ({ description, html_url, language, na
           </Text>
         </Box>
         <Box mb='6'>
-          <Badge borderRadius='full' px='2' mr='2' colorScheme={'blackAlpha'}>
+          <Badge borderRadius='full' px='2' mr='2' colorScheme={colorByLanguage(language)}>
             {language}
           </Badge>
         </Box>
--- src/pages/GitHubProjects.tsx
+++ src/pages/GitHubProjects.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import {
   Text,
   SimpleGrid
@@ -7,12 +7,37 @@ import { Element } from 'react-scroll'
 import IGitHubProject from '../models/IGitHubProject'
 import { getProjects } from '../services/gitHubProjects'
 import CardGitHubProject from '../components/CardGitHubProject'
+import { useToast } from '@chakra-ui/react'
+import scrollReveal from 'scrollreveal'
 
 export default function GitHubProjects () {
   const [gitHubProjects, setGitHubProjects] = useState<IGitHubProject[]>([])
+  const projectsRef = useRef<HTMLDivElement>(null)
+  const toast = useToast()
 
   const setStateGitHubProjects = async () => {
-    setGitHubProjects(await getProjects())
+    try {
+      setGitHubProjects(await getProjects())
+      if (projectsRef.current) {
+        scrollReveal().reveal(projectsRef.current, {
+          delay: 120,
+          distance: '2em',
+          origin: 'bottom',
+          opacity: 0,
+          reset: true
+        })
+      }
+    } catch (error) {
+      toast({
+        title: 'Error del servidor',
+        description: 'Hubo un fallo al cargar los recursos de GitHub.',
+        status: 'error',
+        duration: 3000,
+        variant: 'top-accent',
+        position: 'bottom',
+        isClosable: true,
+      })
+    }
   }
 
   useEffect(() => {
@@ -22,7 +47,7 @@ export default function GitHubProjects () {
   return (
     <Element name='github-projects'>
       <Text align='center' fontSize='3xl' fontWeight='semibold' mt='10vh' mb='4vh'>Proyectos de GitHub</Text>
-      <SimpleGrid columns={[1, 2, 3]} gap={8}>
+      <SimpleGrid columns={[1, 2, 3]} gap={8} ref={projectsRef}>
         {
           gitHubProjects.map(gitHubProject => (
             <CardGitHubProject key={gitHubProject.id} { ...gitHubProject } />
