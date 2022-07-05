1ec6905860b43c3a245076bf8e39444ab679d88f
jemgdev <jemgdev@gmail.com>
2022-07-04 22:29:33 -0500

    UI enhanced. Ver. 1.0.2

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,8 +2,7 @@ import { useRef, useEffect } from 'react'
 import { 
   Text, 
   Container, 
-  Flex,
-  Center
+  Flex
 } from '@chakra-ui/react'
 import { Element } from 'react-scroll'
 import scrollReveal from 'scrollreveal'
@@ -29,8 +28,8 @@ export default function Home () {
     <Element name='home'>
       <Flex direction='column' alignItems='center' justifyContent='center' minH='100vh'>
         <Container maxW='container.md'>
-          <Text fontSize='4xl' ref={textNameRef} mb='4vh'>Hola, soy Josué Emmanuel Medina García</Text>
-          <Text fontSize='xl' ref={textDescriptionRef}>Soy web developer full stack apasionado por el backend. Me encanta desarrollar sistemas con el fin de automatizar procesos en empresas de cualquier rubro.</Text>
+          <Text fontSize={{ base: '4xl', md: '5xl' }} ref={textNameRef} mb='4vh'>Hola, soy Josué Emmanuel Medina García</Text>
+          <Text fontSize={{ base: '2xl', md: '3xl' }} ref={textDescriptionRef}>Soy web developer full stack apasionado por el backend. Me encanta desarrollar sistemas con el fin de automatizar procesos en empresas de cualquier rubro.</Text>
         </Container>
       </Flex>
     </Element>
---
+++ src/services/skills.ts
@@ -0,0 +1,77 @@
+interface skill {
+  src: string,
+  alt: string
+}
+
+const skills: skill[] = [
+  {
+    src: 'https://img.icons8.com/ultraviolet/144/000000/react--v2.png',
+    alt: 'react'
+  },
+  {
+    src: 'https://img.icons8.com/color/144/000000/javascript--v1.png',
+    alt: 'javasrcipt'
+  },
+  {
+    src: 'https://img.icons8.com/color/144/000000/mongodb.png',
+    alt: 'mongodb'
+  },
+  {
+    src: 'https://img.icons8.com/color/144/000000/nodejs.png',
+    alt: 'nodejs'
+  },
+  {
+    src: 'https://img.icons8.com/color/144/000000/java-coffee-cup-logo--v1.png',
+    alt: 'java'
+  },
+  {
+    src: 'https://img.icons8.com/color/144/000000/tomcat.png',
+    alt: 'tomcat'
+  },
+  {
+    src: 'https://img.icons8.com/color/144/000000/html-5--v1.png',
+    alt: 'html'
+  },
+  {
+    src: 'https://img.icons8.com/color/144/000000/css3.png',
+    alt: 'css3'
+  },
+  {
+    src: 'https://img.icons8.com/color/144/000000/linux--v1.png',
+    alt: 'linux'
+  },
+  {
+    src: 'https://img.icons8.com/color/144/000000/mysql-logo.png',
+    alt: 'mysql'
+  },
+  {
+    src: 'https://img.icons8.com/offices/144/000000/php-logo.png',
+    alt: 'php'
+  },
+  {
+    src: 'https://img.icons8.com/color/144/000000/git.png',
+    alt: 'git'
+  },
+  {
+    src: 'https://img.icons8.com/color/144/000000/android-studio--v2.png',
+    alt: 'android-studio'
+  },
+  {
+    src: 'https://img.icons8.com/fluency/144/000000/azure-1.png',
+    alt: 'azure'
+  },
+  {
+    src: 'https://img.icons8.com/color/144/000000/heroku.png',
+    alt: 'heroku'
+  },
+  {
+    src: 'https://img.icons8.com/color/144/000000/typescript.png',
+    alt: 'typescript'
+  },
+  {
+    src: 'https://img.icons8.com/color/144/000000/chakra-ui.png',
+    alt: 'chakra-ui'
+  }
+]
+
+export default skills
