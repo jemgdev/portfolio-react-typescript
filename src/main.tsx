100ee5814c34c70fcbb274eedefa69f102f0867b
Josue Medina <josue.medinag@bam.com.gt>
2025-05-30 12:13:22 -0500

    feat: improve

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
+import theme from './theme'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <ChakraProvider>
-    <ColorModeScript initialColorMode={'dark'} />
+  <ChakraProvider theme={theme}>
+    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
     <App />
   </ChakraProvider>
 )
