710b8657b18d10e6a9f9b6eb7ce2065eac68154a
Josue Medina <josue.medinag@bam.com.gt>
2025-05-30 02:43:39 -0500

    feat: improve navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -20,49 +20,46 @@ export default function Simple() {
   const { colorMode, toggleColorMode } = useColorMode()
 
   return (
-    <>
-      <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4} className='mix'>
-        <Container maxW='container.xl'>
-          <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
-            <IconButton
-              size={'md'}
-              icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
-              aria-label={'Open Menu'}
-              display={{ md: 'none' }}
-              onClick={onToggle}
-            />
-            <HStack spacing={8} alignItems={'center'}>
-              <Text as={'a'} fontSize='2xl' fontWeight='semibold' href='https://portfolio.jemgdev.click'>jemgdev</Text>
-              <HStack
-                as={'nav'}
-                spacing={4}
-                display={{ base: 'none', md: 'flex' }}>
-                <NavLink href='#home' text='Inicio' scrollToElement='home' onClose={onClose} />
-                <NavLink href='#projects' text='Proyectos' scrollToElement='projects' onClose={onClose}/>
-                <NavLink href='#github-projects' text='Proyectos de github' scrollToElement='github-projects' onClose={onClose}/>
-                <NavLink href='#skills' text='Skills' scrollToElement='skills' onClose={onClose}/>
-                <NavLink href='#contact-me' text='Contáctame' scrollToElement='contact-me' onClose={onClose}/>
-              </HStack>
-            </HStack>
-            <Flex alignItems='end'>
-              <Button onClick={toggleColorMode}>
-                {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
-              </Button>
-            </Flex>
-          </Flex>
-        </Container>
-        <Collapse in={isOpen}>
-          <Box pb={4} display={{ md: 'none' }}>
-            <Stack as={'nav'} spacing={4}>
-              <NavLink href='#home' text='Inicio' scrollToElement='home' onClose={onClose}/>
+    <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4} boxShadow="md" position="sticky" top="0" zIndex="1000">
+      <Container maxW='container.xl'>
+        <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+          <IconButton
+            size={'md'}
+            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+            aria-label={'Open Menu'}
+            display={{ md: 'none' }}
+            onClick={onToggle}
+          />
+          <HStack spacing={8} alignItems={'center'}>
+            <Text as={'a'} fontSize='2xl' fontWeight='semibold' href='https://portfolio.jemgdev.click'>jemgdev</Text>
+            <HStack
+              as={'nav'}
+              spacing={4}
+              display={{ base: 'none', md: 'flex' }}>
+              <NavLink href='#home' text='Inicio' scrollToElement='home' onClose={onClose} />
               <NavLink href='#projects' text='Proyectos' scrollToElement='projects' onClose={onClose}/>
-              <NavLink href='#github-projects' text='Proyectos de github' scrollToElement='github-projects' onClose={onClose}/>
               <NavLink href='#skills' text='Skills' scrollToElement='skills' onClose={onClose}/>
               <NavLink href='#contact-me' text='Contáctame' scrollToElement='contact-me' onClose={onClose}/>
-            </Stack>
-          </Box>
-        </Collapse>
-      </Box>
-    </>
+            </HStack>
+          </HStack>
+          <Flex alignItems='end'>
+            <Button onClick={toggleColorMode}>
+              {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
+            </Button>
+          </Flex>
+        </Flex>
+      </Container>
+      <Collapse in={isOpen}>
+        <Box pb={4} display={{ md: 'none' }}>
+          <Stack as={'nav'} spacing={4}>
+            <NavLink href='#home' text='Inicio' scrollToElement='home' onClose={onClose}/>
+            <NavLink href='#projects' text='Proyectos' scrollToElement='projects' onClose={onClose}/>
+            <NavLink href='#github-projects' text='Proyectos de github' scrollToElement='github-projects' onClose={onClose}/>
+            <NavLink href='#skills' text='Skills' scrollToElement='skills' onClose={onClose}/>
+            <NavLink href='#contact-me' text='Contáctame' scrollToElement='contact-me' onClose={onClose}/>
+          </Stack>
+        </Box>
+      </Collapse>
+    </Box>
   )
 }
